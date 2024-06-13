e006c81f5a92a72fd825c72f34ac49be5b0d13ac
Guy Moalem <moalemm@post.bgu.ac.il>
2024-06-14 01:21:22 +0300

    done task 2A

---
+++ lab_3/task2/main.c
@@ -0,0 +1,34 @@
+#include "util.h"
+
+#define SYS_WRITE 4
+#define STDOUT 1
+#define SYS_OPEN 5
+#define O_RDWR 2
+#define SYS_SEEK 19
+#define SEEK_SET 0
+#define SHIRA_OFFSET 0x291
+
+extern int system_call();
+extern int infection();
+extern int infector();
+
+int main(int argc, char *argv[], char *envp[])
+{
+    unsigned int i;
+    char *filename = 0;
+
+    for (i = 1; i < argc; i++)
+    {
+        if (!strncmp(argv[i], "-a", 2))
+        {
+            filename = argv[i] + 2;
+            system_call(SYS_WRITE, STDOUT, filename, strlen(filename));
+            system_call(SYS_WRITE, STDOUT, "\n", 1);
+        }
+    }
+
+    infection();
+    infector(filename);
+
+    return 0;
+}
